ef085b30add11d30550549da8f5b979209d5fcbf
karthik <karthikbangera87@gmail.com>
2015-10-31 19:53:20 -0400

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,30 @@
+import webapp2
+import datetime
+import os
+import jinja2
+
+from google.appengine.api import users
+
+template_env = jinja2.Environment(
+        loader = jinja2.FileSystemLoader(os.getcwd()))
+
+class Hello(webapp2.RequestHandler):
+    def get(self):
+        current_time = datetime.datetime.now()
+        user = users.get_current_user()
+        login_url = users.create_login_url(self.request.path)
+        logout_url = users.create_logout_url(self.request.path)
+        template = template_env.get_template('home.html')
+        context = {
+
+        	'current_time':current_time,
+        	'user': user,
+        	'login_url':login_url,
+        	'logout_url':logout_url
+        }
+        self.response.write(template.render(context))
+
+
+app = webapp2.WSGIApplication(
+
+  [('/',Hello)],debug=True)
